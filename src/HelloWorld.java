78c771c240ab0204336d59dc28ae53037d5f750b
Azel <96758369+shine816@users.noreply.github.com>
2022-12-15 10:04:28 +0800

    服务器修改

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
         System.out.println("hot-fix");
         System.out.println("I am master");
         System.out.println("I am hot fix");
+        System.out.println("I am server");
     }
 }
