afb08826636cf09d84f514b0989f33495667cc40
shine <1971513618@qq.com>
2022-12-15 09:39:48 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author lixiang
+ * @create 2022-12-15 9:38
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
